7ed8cd77ef941f66e3bd4b8481126b18bda34c87
damien <doute.d@gmail.com>
2020-07-30 16:28:57 -0400

    some cleanup

--- zomes/snapmail/src/chunk.rs
+++ zomes/snapmail/src/chunk.rs
@@ -7,13 +7,6 @@ use holo_hash::hash_type;
 
 holochain_wasmer_guest::holochain_externs!();
 
-// use hdk::{
-//     entry_definition::ValidatingEntryType,
-//     holochain_persistence_api::{
-//         cas::content::Address, hash::HashString,
-//     },
-// };
-
 pub const CHUNK_MAX_SIZE: usize = 200 * 1024;
 
 map_extern!(write_chunk, _write_chunk);
@@ -128,30 +121,23 @@ pub fn _write_chunk(
     let data_hash = "QM324rdx".to_string();
     let chunk_index = 0;
     let initial_file = FileChunk::new(data_hash.clone(), chunk_index, chunk.0);
-    // let file_entry = Entry::App(entry_kind::FileChunk.into(), initial_file.into());
-    // let maybe_file_address = hdk::commit_entry(&file_entry);
 
-    debug!(format!("data_hash: {}", data_hash)).ok();
+    //debug!(format!("data_hash: {}", data_hash)).ok();
 
     let res: EntryHash = host_call!(
         __commit_entry,
         CommitEntryInput::new(((&initial_file).into(), (&initial_file).try_into()?))
     )?;
     debug!(format!("commit_result: {:?}", res)).ok();
-    debug!(format!("commit_result: {:?}", res.hash_type())).ok();
-    debug!(format!("commit_result: {:?}", res.get_raw())).ok();
     Ok(res)
 }
 
 /// Zome function
 /// Get chunk index and chunk as base64 string in local source chain at given address
-pub fn _get_chunk(chunk_address_str: /*EntryHash*/ MyRaw) -> Result<MyString, WasmError> {
-    debug!(format!("!!! getChunk : {:?}", chunk_address_str)).ok();
-    //let hardcoded = [71, 198, 64, 23, 140, 236, 238, 52, 45, 24, 23, 49, 174, 76, 245, 96, 159, 177, 79, 237, 236, 216, 152, 112, 146, 158, 213, 243, 212, 178, 164, 145, 204, 155, 174, 205];
-    let chunk_address = HoloHash::<hash_type::Entry>::from_raw_bytes_and_type(chunk_address_str.to_vec(), hash_type::Entry::Content);
-    //let chunk_address = EntryHash::new();
+pub fn _get_chunk(chunk_address_raw: /*EntryHash*/ MyRaw) -> Result<MyString, WasmError> {
+    debug!(format!("chunk_address_raw: {:?}", chunk_address_raw)).ok();
+    let chunk_address = HoloHash::<hash_type::Entry>::from_raw_bytes_and_type(chunk_address_raw.to_vec(), hash_type::Entry::Content);
         debug!(format!("chunk_address: {:?}", chunk_address)).ok();
-    //hdk::debug(format!("get_chunk(): {}", chunk_address)).ok();
     let maybe_entry = get_entry!(chunk_address)
         .expect("No reason for get_entry() to crash");
     if maybe_entry.is_none() {
@@ -162,32 +148,6 @@ pub fn _get_chunk(chunk_address_str: /*EntryHash*/ MyRaw) -> Result<MyString, Wa
         Entry::App(entry_value) => FileChunk::try_from(entry_value).expect("should convert"),
         _ =>  return Ok(MyString(String::new().into())),
     };
-
     // Ok((chunk.chunk_index, chunk.chunk))
     Ok(MyString(chunk.chunk.into()))
 }
-
-//
-// #[no_mangle]
-// pub extern "C" fn _get_chunk(ptr: GuestPtr) -> GuestPtr {
-//     // Convert input
-//     let input: HostInput = host_args!(ptr);
-//     let sb: SerializedBytes = input.into_inner();
-//     let chunk_address: EntryHash = match EntryHash::try_from(input.into_inner()) {
-//
-//     };
-//
-//     // Do actual processing
-//     let maybe_chunk = __get_chunk(chunk_address);
-//
-//     // Convert ouput
-//     return match maybe_chunk {
-//         Ok(chunk) => {
-//             let response_sb: SerializedBytes = try_result!(chunk.try_into(), "failed to serialize chunk");
-//             ret!(GuestOutput::new(response_sb));
-//         },
-//         Err(msg) => {
-//             ret_err!(msg);
-//         }
-//     };
-// }
