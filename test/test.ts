04993bb56a30cb97fed498191f657273ab48fd3c
damien <doute.d@gmail.com>
2020-07-31 12:55:48 -0400

    get_chunk_hash() and send_chunk()

--- test/test.ts
+++ test/test.ts
@@ -10,36 +10,79 @@ const config = Config.gen({
 
 const orchestrator = new Orchestrator()
 
-orchestrator.registerScenario('call zome', async (s, t) => {
-    const { alex } = await s.players({ alex: config })
-    await alex.spawn()
+// orchestrator.registerScenario('write/get chunk', async (s, t) => {
+//     const { alex } = await s.players({ alex: config })
+//     await alex.spawn()
+//
+//     //const result = await alex.call('tester', 'snapmail', 'foo', { anything: 'goes' })
+//     //const result = await alex.call('tester', 'snapmail', 'whoami', undefined)
+//     //console.log('agent_pubkey:', result.agent_pubkey.hash.toString())
+//     const data_string = "0123465789".repeat(10 * 1024 * 1024 / 10)
+//     //const data_string = "toto"
+//     const chunk = {
+//         data_hash: "Qm324rdx",
+//         chunk_index: 0,
+//         chunk: data_string,
+//     };
+//     //const data_string = "toto";
+//     let result = await alex.call('tester', 'snapmail', 'write_chunk', chunk)
+//     console.log('result0:', result)
+//     result = await alex.call('tester', 'snapmail', 'get_chunk_hash', chunk)
+//     console.log('result1:', result)
+//     let entry_hash = [...result.hash];
+//     console.log('result1 hash:', entry_hash)
+//     //t.equal(result, 'foo')
+//
+//     // Get Entry
+//     // =========
+//     let arg = {
+//         hash: {"type": "Buffer", "data": [71,198,64,23,140,236,238,52,45,24,23,49,174,76,245,96,159,177,79,237,236,216,152,112,146,158,213,243,212,178,164,145,204,155,174,205]},// result,
+//         hash_type: {'1': null},
+//     }
+//     //console.log('arg:', arg)
+//     const result2 = await alex.call('tester', 'snapmail', 'get_chunk', entry_hash)
+//     console.log('result2:', result2)
+//     t.equal(result2, data_string)
+// })
 
-    //const result = await alex.call('tester', 'snapmail', 'foo', { anything: 'goes' })
-    //const result = await alex.call('tester', 'snapmail', 'whoami', undefined)
-    //console.log('agent_pubkey:', result.agent_pubkey.hash.toString())
-    const data_string = "0123465789".repeat(1 * 1024 * 1024 / 10)
+orchestrator.registerScenario('send chunk', async (s, t) => {
+    const { alex, billy } = await s.players({ alex: config, billy: config })
+    await alex.spawn()
+    await billy.spawn()
+
+    //console.log({billy})
+    //console.log("billy: " + JSON.stringify(billy));
+    //console.log("public_address: " + billy._conductor.agents[0].public_address);
+
+    const billyInfo = await billy.call('tester', 'snapmail', 'whoami', undefined)
+    console.log('agent_pubkey: ', billyInfo.agent_pubkey.hash)
+
+    //const data_string = "0123465789".repeat(100 * 1024 * 1024 / 10)
+    const data_string = "toto";
     const chunk = {
         data_hash: "Qm324rdx",
         chunk_index: 0,
         chunk: data_string,
     };
-    //const data_string = "toto";
-    const result = await alex.call('tester', 'snapmail', 'write_chunk', chunk)
-    console.log('result1:', result)
+    const sendChunk = {
+        agent_pubkey: billyInfo.agent_pubkey,
+        file_chunk: chunk,
+    }
+    console.log('sendChunk: ', sendChunk)
+    const result = await alex.call('tester', 'snapmail', 'send_chunk', sendChunk)
+    console.log('result1: ', result)
     let entry_hash = [...result.hash];
     console.log('result1 hash:', entry_hash)
     //t.equal(result, 'foo')
 
-    // Get Entry
-    // =========
-    let arg = {
-        hash: {"type": "Buffer", "data": [71,198,64,23,140,236,238,52,45,24,23,49,174,76,245,96,159,177,79,237,236,216,152,112,146,158,213,243,212,178,164,145,204,155,174,205]},// result,
-        hash_type: {'1': null},
-    }
-    //console.log('arg:', arg)
-    const result2 = await alex.call('tester', 'snapmail', 'get_chunk', entry_hash)
-    console.log('result2:', result2)
-    t.equal(result2, data_string)
+    // await s.consistency()
+    //
+    // // Get Entry
+    // // =========
+    // //console.log('arg:', arg)
+    // const result2 = await billy.call('tester', 'snapmail', 'get_chunk', entry_hash)
+    // console.log('result2:', result2)
+    // t.equal(result2, data_string)
 })
 
 orchestrator.run()
--- zomes/snapmail/src/chunk.rs
+++ zomes/snapmail/src/chunk.rs
@@ -6,11 +6,14 @@ use holo_hash::HoloHash;
 use holo_hash::hash_type;
 
 holochain_wasmer_guest::holochain_externs!();
+holochain_wasmer_guest::host_externs!(__call_remote);
 
-pub const CHUNK_MAX_SIZE: usize = 200 * 1024;
+pub const CHUNK_MAX_SIZE: usize = 10 * 1024 * 1024;
 
 map_extern!(write_chunk, _write_chunk);
 map_extern!(get_chunk, _get_chunk);
+map_extern!(send_chunk, _send_chunk);
+map_extern!(get_chunk_hash, _get_chunk_hash);
 
 #[derive(Shrinkwrap, Clone, Debug, PartialEq, Default, Serialize, Deserialize, SerializedBytes)]
 pub struct MyString(String);
@@ -38,6 +41,12 @@ impl FileChunk {
             crdt_type: CrdtType,
             required_validations: 8.into(),
         }
+    }
+}
+
+impl From<FileChunk> for EntryDefId {
+    fn from(_: FileChunk) -> Self {
+        "file_chunk".into()
     }
 }
 
@@ -114,21 +123,30 @@ impl FileChunk {
 /// Zome function
 /// Write base64 file as string to source chain
 pub fn _write_chunk(
-    fileChunk: FileChunk
-) -> Result<EntryHash, WasmError> {
-    //debug!(format!("fileChunk: {:?}", fileChunk)).ok();
-    let res: EntryHash = host_call!(
-        __commit_entry,
-        CommitEntryInput::new(((&fileChunk).into(), (&fileChunk).try_into()?))
-    )?;
+    file_chunk: FileChunk
+) -> Result<HeaderHash, WasmError> {
+    debug!(format!("fileChunk: {:?}", file_chunk)).ok();
+    let res = commit_entry!(file_chunk.clone())?;
     debug!(format!("commit_result: {:?}", res)).ok();
     Ok(res)
 }
 
 /// Zome function
+pub fn _get_chunk_hash(
+    file_chunk: FileChunk
+) -> Result<EntryHash, WasmError> {
+    debug!(format!("fileChunk: {:?}", file_chunk)).ok();
+    let res = entry_hash!(file_chunk.clone())?;
+    debug!(format!("entry_hash_result: {:?}", res)).ok();
+    Ok(res)
+}
+
+
+/// Zome function
 /// Get chunk index and chunk as base64 string in local source chain at given address
-pub fn _get_chunk(chunk_address_raw: /*EntryHash*/ MyRaw) -> Result<MyString, WasmError> {
-    debug!(format!("chunk_address_raw: {:?}", chunk_address_raw)).ok();
+pub fn _get_chunk(chunk_address_raw: MyRaw) -> Result<MyString, WasmError> {
+//pub fn _get_chunk(chunk_address: EntryHash) -> Result<MyString, WasmError> {
+        debug!(format!("chunk_address_raw: {:?}", chunk_address_raw)).ok();
     let chunk_address = HoloHash::<hash_type::Entry>::from_raw_bytes_and_type(chunk_address_raw.to_vec(), hash_type::Entry::Content);
         debug!(format!("chunk_address: {:?}", chunk_address)).ok();
     let maybe_entry = get_entry!(chunk_address)
@@ -143,4 +161,27 @@ pub fn _get_chunk(chunk_address_raw: /*EntryHash*/ MyRaw) -> Result<MyString, Wa
     };
     // Ok((chunk.chunk_index, chunk.chunk))
     Ok(MyString(chunk.chunk.into()))
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize, SerializedBytes)]
+pub struct SendChunkInput {
+    pub agent_pubkey: AgentPubKey,
+    pub file_chunk: FileChunk,
+}
+
+//
+fn _send_chunk(input: SendChunkInput) -> Result<EntryHash, WasmError> {
+    let zome_name = zome_info!()?.zome_name;
+    debug!(format!("to_agent: {:?}", input.agent_pubkey)).ok();
+    let chunk = input.file_chunk.try_into()?;
+    debug!(format!("chunk: {:?}", chunk)).ok();
+    let result: SerializedBytes = call_remote!(
+        input.agent_pubkey,
+        zome_name,
+        "write_chunk".to_string(),
+        CapSecret::default(),
+        chunk
+    )?;
+
+    Ok(result.try_into()?)
 }
