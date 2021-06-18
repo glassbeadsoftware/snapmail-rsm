186b6dac3b52e6eef2a7b26469ce341c5008687a
damien <doute.d@gmail.com>
2021-06-18 16:19:34 -0400

    Moved and fixed packing scripts

--- scripts/pack-happ.sh
+++ scripts/pack-happ.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 # Compile the WASM
 cargo build --release --target wasm32-unknown-unknown
 # Pack DNAs
 hc dna pack --output=snapmail.dna snapmail.dna.workdir
 # Pack the Happ with everything
-hc app pack --output=snapmail.happ .
+hc app pack --output=snapmail.happ snapmail.dna.workdir
