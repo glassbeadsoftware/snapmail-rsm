6d19cf72737e0ea8fd9f1159f300f2791e8235da
damien <doute.d@gmail.com>
2022-02-04 14:02:08 -0500

    Implemented get_outmail_delivery_state() (untested though)

--- zomes/snapmail/src/mail/functions/get_outmail_state.rs
+++ zomes/snapmail/src/mail/functions/get_outmail_state.rs
@@ -53,6 +53,61 @@ pub fn get_outmail_state(outmail_hh: HeaderHash) -> ExternResult<OutMailState> {
 }
 
 
+use std::collections::HashMap;
+
+
+/// Get full state of an OutMail
+#[hdk_extern]
+#[snapmail_api]
+pub fn get_outmail_delivery_state(outmail_hh: HeaderHash) -> ExternResult<HashMap<AgentPubKey, DeliveryState>> {
+   debug!(" *** get_outmail_delivery_state(): ");
+   /// Get OutMail Details
+   let maybe_details = get_details(outmail_hh.clone(), GetOptions::latest())?;
+   if maybe_details.is_none() {
+      return error("No OutMail at given address");
+   }
+   let el_details = match maybe_details.unwrap() {
+      Details::Element(details) => details,
+      Details::Entry(_) => unreachable!("in get_outmail_state()"),
+   };
+   /// Get OutMail Entry
+   let outmail: OutMail = get_typed_from_el(el_details.element.clone())
+      ?;
+   let outmail_eh = el_details.element.header().entry_hash().expect("Should have an Entry");
+   /// Grab links
+   let pendings = get_links(outmail_eh.clone(), LinkKind::Pendings.as_tag_opt())?;
+   let sents = get_links(outmail_eh.clone(), LinkKind::Sents.as_tag_opt())?;
+   let receipts = get_links(outmail_eh.clone(), LinkKind::Receipt.as_tag_opt())?;
+   /// Transform links into agent lists
+   let pending_agents: Vec<AgentPubKey> = pendings.iter().map(|link| {
+      LinkKind::Pendings.unconcat_hash(&link.tag).unwrap()
+   }).collect();
+   let sent_agents: Vec<AgentPubKey> = sents.iter().map(|link| {
+      LinkKind::Sents.unconcat_hash(&link.tag).unwrap()
+   }).collect();
+   let receipt_agents: Vec<AgentPubKey> = receipts.iter().map(|link| {
+      LinkKind::Receipt.unconcat_hash(&link.tag).unwrap()
+   }).collect();
+   /// Determine state of delivery for each recipient and insert result in hashmap
+   let mut map = HashMap::new();
+   for recipient in outmail.recipients() {
+      let mut state = DeliveryState::Unsent;
+      if receipt_agents.contains(&recipient) {
+         state = DeliveryState::Acknowledged
+      }
+      if pending_agents.contains(&recipient) {
+         state = DeliveryState::Sent
+      }
+      if sent_agents.contains(&recipient) {
+         state = DeliveryState::Sent
+      }
+      map.insert(recipient, state);
+   }
+   /// Done
+   Ok(map)
+}
+
+
 // /// Delete Pendings links from outmail to `to` agent
 // fn delete_pendings_link(outmail_eh: &EntryHash, to: &AgentPubKey) -> ExternResult<HeaderHash> {
 //    let pendings_links_result = get_links(
