34c87cc83b0b39c07e76a7fba237043886459234
damien <doute.d@gmail.com>
2022-03-24 14:21:12 -0400

    Remove holochain rev scripts

--- scripts/write-zome-hash.bat
+++
@@ -1,8 +0,0 @@
-REM Compile the WASM
-cargo build --release --target wasm32-unknown-unknown
-REM Compile the exe
-cargo build --package wasm_utils
-REM Compute hash of zome
-.\target\debug\hash_zome  > zome_hash.txt
-REM Show hash
-.\target\debug\hash_zome
