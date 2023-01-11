f6bc784ce36f8c57126ed4e2e529893de66a2ae8
damien <doute.d@gmail.com>
2023-01-11 13:45:16 -0500

    upgrade to holochain beta-rc.2

--- scripts/pack-happ.bat
+++ scripts/pack-happ.bat
@@ -1,6 +1,6 @@
 REM Compile the WASM
 cargo build --release --target wasm32-unknown-unknown
 REM Pack DNAs
-hc dna pack workdir
+hc dna pack workdir -o workdir/snapmail.dna
 REM Pack the Happ with everything
-hc app pack workdir
+hc app pack workdir -o workdir/snapmail.happ
--- scripts/ts-bindings.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cargo test
-sed -i '1d' zomes/snapmail_model/bindings/*.ts
-cat zomes/snapmail_model/bindings/*.ts > ./bindings/snapmail.bindings.ts
