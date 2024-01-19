d4b43185da46366196011d14bf763d7bb92edd89
damien <doute.d@gmail.com>
2024-01-19 11:10:26 -0500

    added zome & role name consts

--- zomes/snapmail_model/src/constants.rs
+++ zomes/snapmail_model/src/constants.rs
@@ -1,4 +1,7 @@
-pub const ZOME_NAME: &str = "snapmail";
+
+pub const SNAPMAIL_DEFAULT_INTEGRITY_ZOME_NAME: &'static str = "snapmail_model";
+pub const SNAPMAIL_DEFAULT_COORDINATOR_ZOME_NAME: &'static str = "snapmail";
+pub const SNAPMAIL_DEFAULT_ROLE_NAME: &'static str = "rSnapmail";
 
 pub const DIRECT_SEND_TIMEOUT_MS: usize = 1000;
 pub const DIRECT_SEND_CHUNK_TIMEOUT_MS: usize = 10000;
