3ce45e4d86c1fa153dd56459635218b384efa0d5
damien <doute.d@gmail.com>
2024-03-29 11:40:25 -0400

    disable link validation

--- zomes/snapmail_model/src/validate.rs
+++ zomes/snapmail_model/src/validate.rs
@@ -45,31 +45,32 @@ pub fn validate_create_entry(creation_action: EntryCreationAction, entry: Entry)
 
 /// Checks Agent Link is created by self
 pub fn validate_create_link(create_link: HoloHashed<CreateLink>, signature: Signature) -> ExternResult<ValidateCallbackResult>  {
-   debug!("validate_create_link(): {:?}", create_link);
-   /// Retrieve Path::Component from LinkTag
-   let tag_bytes = create_link.tag.clone().into_inner();
-   let unsafe_bytes = UnsafeBytes::from(tag_bytes.clone());
-   let ser_bytes = SerializedBytes::from(unsafe_bytes);
-   let maybe_component = Component::try_from(ser_bytes);
-   let Ok(component) = maybe_component else {
-      return Ok(ValidateCallbackResult::Invalid("Failed to convert LinkTag to Component".to_string()))
-   };
-   /// Retrieve AgentPubKey from Component
-   let maybe_agent_key = AgentPubKey::from_raw_39(component.as_ref().to_vec());
-   //debug!("validate_agent_link(): agent_key = {:?}", maybe_agent_key);
-   /// Check key in LinkTag matches author and action signature
-   let Ok(agent_key) = maybe_agent_key else {
-      /// TODO: Path root is also of type Agent but does not have the LinkTag, so skip for now.
-      // return Ok(ValidateCallbackResult::Invalid("Failed to convert Component to AgentPubKey".to_string()))
-      return Ok(ValidateCallbackResult::Valid);
-   };
-   if agent_key != create_link.author {
-      return Ok(ValidateCallbackResult::Invalid("Link Author and Tag don't match".to_string()))
-   }
-   let success = verify_signature(agent_key, signature, Action::CreateLink(create_link.content))?;
-   Ok(if !success {
-      ValidateCallbackResult::Invalid("Failed to verify signature".to_string())
-   } else {
-      ValidateCallbackResult::Valid
-   })
+   // debug!("validate_create_link(): {:?}", create_link);
+   // /// Retrieve Path::Component from LinkTag
+   // let tag_bytes = create_link.tag.clone().into_inner();
+   // let unsafe_bytes = UnsafeBytes::from(tag_bytes.clone());
+   // let ser_bytes = SerializedBytes::from(unsafe_bytes);
+   // let maybe_component = Component::try_from(ser_bytes);
+   // let Ok(component) = maybe_component else {
+   //    return Ok(ValidateCallbackResult::Invalid("Failed to convert LinkTag to Component".to_string()))
+   // };
+   // /// Retrieve AgentPubKey from Component
+   // let maybe_agent_key = AgentPubKey::from_raw_39(component.as_ref().to_vec());
+   // //debug!("validate_agent_link(): agent_key = {:?}", maybe_agent_key);
+   // /// Check key in LinkTag matches author and action signature
+   // let Ok(agent_key) = maybe_agent_key else {
+   //    /// TODO: Path root is also of type Agent but does not have the LinkTag, so skip for now.
+   //    // return Ok(ValidateCallbackResult::Invalid("Failed to convert Component to AgentPubKey".to_string()))
+   //    return Ok(ValidateCallbackResult::Valid);
+   // };
+   // if agent_key != create_link.author {
+   //    return Ok(ValidateCallbackResult::Invalid("Link Author and Tag don't match".to_string()))
+   // }
+   // let success = verify_signature(agent_key, signature, Action::CreateLink(create_link.content))?;
+   // Ok(if !success {
+   //    ValidateCallbackResult::Invalid("Failed to verify signature".to_string())
+   // } else {
+   //    ValidateCallbackResult::Valid
+   // })
+   Ok(ValidateCallbackResult::Valid)
 }
